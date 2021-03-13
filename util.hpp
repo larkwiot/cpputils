625e6e1e3f92f528a631bd318cf073ba35be92e8
larkwiot <ltfragor@gmail.com>
2021-03-12 20:47:38 -0500

    add string utils

--- util.hpp
+++ util.hpp
@@ -1,15 +1,45 @@
 #include <vector>
 #include <string>
-#include <string>
+#include <concepts>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
 
-// string split
-// string join
+#ifndef LTSTAFFEL_UTIL_HPP
+#define LTSTAFFEL_UTIL_HPP
+
+namespace lt {
+
+template <char delim>
+std::vector<std::string> split(std::string& str) {
+  std::vector<std::string> fields {};
+  std::string l;
+
+  while (std::getline(str, l, delim)) {
+    fields.emplace_back(std::move(l));
+  }
+
+  return fields;
+}
+
+template <char delim>
+std::string join(std::vector<std::string> const& vec) {
+  std::string result {vec.at(0)};
+
+  for (auto s : vec) {
+    result += delim += s;
+  }
+
+  return result;
+}
 
 // int-type to byte vector
 
+
 // read file bytes
-// read file lines
-std::vector<std::string> read_file_lines(std::string fn) {
+
+
+std::vector<std::string> read_file_lines(std::string& fn) {
   std::ifstream fh {fn};
 
   if (!fh) {
@@ -17,7 +47,7 @@ std::vector<std::string> read_file_lines(std::string fn) {
     abort();
   }
 
-  auto lines = std::vector<std::string> {};
+  std::vector<std::string> lines {};
 
   std::string l;
   while (std::getline(fh, l)) {
@@ -26,3 +56,7 @@ std::vector<std::string> read_file_lines(std::string fn) {
   
   return lines;
 }
+
+}
+
+#endif
