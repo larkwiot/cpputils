c50e8102a9d5792cbd437be2fba1218def84aefe
larkwiot <ltfragor@gmail.com>
2021-05-06 21:47:58 -0400

    finish in-progress funcs and write baseline tests

---
+++ test.cpp
@@ -0,0 +1,45 @@
+#include "util.hpp"
+#include "lib/ut.hpp"
+
+using namespace boost::ut;
+
+int main() {
+  "split"_test = [] {
+    std::string test_str1 = "hello world!";
+    std::vector<std::string> test_str1_res1 = {"hello", "world!"};
+    std::vector<std::string> test_str1_res2 = {"hell", " w", "rld!"};
+    expect(lt::split<' '>(test_str1) == test_str1_res1);
+    expect(lt::split<'o'>(test_str1) == test_str1_res2);
+  };
+
+  "join"_test = [] {
+    std::string test_str1 = "hello world!";
+    std::vector<std::string> test_str1_res1 = {"hello", "world!"};
+    std::vector<std::string> test_str1_res2 = {"hell", " w", "rld!"};
+    expect(lt::join<' '>(test_str1_res1) == test_str1);
+    expect(lt::join<'o'>(test_str1_res2) == test_str1);
+  };
+
+  "num_to_bytes"_test = [] {
+    std::vector<u_char> test_byte1_res1 {0, 0, 0, 0xff};
+    std::vector<u_char> test_byte1_res2 {0, 0, 0xff, 0};
+    std::vector<u_char> test_byte1_res3 {0, 0xff, 0, 0};
+    std::vector<u_char> test_byte1_res4 {0xff, 0, 0, 0};
+    expect(lt::num_to_bytes<0xffu>() == test_byte1_res1);
+    expect(lt::num_to_bytes<0xff00u>() == test_byte1_res2);
+    expect(lt::num_to_bytes<0xff0000u>() == test_byte1_res3);
+    expect(lt::num_to_bytes<0xff000000u>() == test_byte1_res4);
+  };
+
+  "read_file_bytes"_test = [] {
+    std::vector<u_char> test_file1_res1 {73, 32, 97, 109, 32, 97, 32, 116, 101, 115, 116};
+    std::string test_file1 = "test_file.txt";
+    expect(lt::read_file_bytes(test_file1) == test_file1_res1);
+  };
+
+  "read_file_lines"_test = [] {
+    std::vector<std::string> test_file_res1 {"I am a test"};
+    std::string test_file1 = "test_file.txt";
+    expect(lt::read_file_lines(test_file1) == test_file_res1);
+  };
+}
--- util.hpp
+++ util.hpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <cstddef>
+#include <sstream>
 
 #ifndef LTSTAFFEL_UTIL_HPP
 #define LTSTAFFEL_UTIL_HPP
@@ -12,10 +14,11 @@ namespace lt {
 
 template <char delim>
 std::vector<std::string> split(std::string& str) {
+  std::stringstream str_stream {str};
   std::vector<std::string> fields {};
   std::string l;
 
-  while (std::getline(str, l, delim)) {
+  while (std::getline(str_stream, l, delim)) {
     fields.emplace_back(std::move(l));
   }
 
@@ -24,20 +27,61 @@ std::vector<std::string> split(std::string& str) {
 
 template <char delim>
 std::string join(std::vector<std::string> const& vec) {
-  std::string result {vec.at(0)};
+  std::string result {vec.front()};
 
-  for (auto s : vec) {
-    result += delim += s;
+  auto end = vec.end();
+  for (auto si = vec.begin() + 1; si != end; ++si) {
+    result += delim;
+    result += *si;
   }
 
   return result;
 }
 
-// int-type to byte vector
+template <typename T>
+concept Byteable = std::unsigned_integral<T>;
 
+template <Byteable auto n>
+std::vector<u_char> num_to_bytes() {
+  auto x = n;
+  std::vector<u_char> result {};
+
+  std::copy(static_cast<const char*>(static_cast<const void*>(&x)),
+            static_cast<const char*>(static_cast<const void*>(&x)) + sizeof(n),
+            std::back_inserter(result));
+
+  std::reverse(result.begin(), result.end());
+
+  return result;
+}
 
 // read file bytes
+std::vector<u_char> read_file_bytes(std::string& fn) {
+  std::ifstream fh {fn, std::ios::binary | std::ios::ate};
 
+  if (!fh) {
+    std::cerr << "[!] error: could not open " << fn << "\n";
+    abort();
+  }
+
+  auto end = fh.tellg();
+  fh.seekg(0, std::ios::beg);
+
+  auto size = std::size_t(end - fh.tellg());
+
+  if (size == 0) {
+    return {};
+  }
+
+  std::vector<u_char> bytes(size);
+
+  if (!fh.read(reinterpret_cast<char*>(bytes.data()), bytes.size())) {
+    std::cerr << "[!] error: could not read " << fn << "\n";
+    abort();
+  }
+
+  return bytes;
+}
 
 std::vector<std::string> read_file_lines(std::string& fn) {
   std::ifstream fh {fn};
